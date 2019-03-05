55777a03d6c37d264b09041a4b7c562253cd8389
Amarnath <kanath10@gmail.com>
2019-03-05 11:54:38 +0530

    Initial commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest
+{
+
+    @Test
+    public void NativeApp()
+    {
+      System.out.println("NativeApp");
+    }
+
+
+    @Test
+    public void IOSApp()
+    {
+      System.out.println("IOSApp");
+    }
+
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest
+{
+   @Test
+   public void BroswerAutomation()
+   {
+
+      System.out.println("BrowserAutomation");
+   }
+
+  
+   @Test
+   public void ElementsUI()
+   {
+      System.out.println("ElementsUI");
+   }
+
+}
